79fc3f29dacafc91d1eb67ccaaaec20082c29a05
Bo Wauters <79152550+Bokoloco@users.noreply.github.com>
2026-08-24 14:21:31 +0200

    Added a line to know where you are poiinting

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -13,9 +13,13 @@ public class BallMovement : MonoBehaviour
 
     private Rigidbody _rb;
 
+    private LineRenderer _lineRenderer;
+
     private float _leftBtnValue;
     private Vector2 _startPositionValue;
     private Vector2 _endPositionValue;
+
+    private bool _ShouldDrawLine;
 
     private void OnEnable()
     {
@@ -41,6 +45,10 @@ public class BallMovement : MonoBehaviour
     {
         _startPositionValue = _positionAction.ReadValue<Vector2>();
 
+        _ShouldDrawLine = true;
+
+        _lineRenderer.enabled = true;
+
         Debug.Log("Start: " + _startPositionValue);
     }
 
@@ -48,24 +56,58 @@ public class BallMovement : MonoBehaviour
     {
         _endPositionValue = _positionAction.ReadValue<Vector2>();
 
-        Debug.Log("End: " + _endPositionValue);
-
         Vector2 difference = _endPositionValue - _startPositionValue;
 
         Vector3 test = new Vector3(difference.x, 0, difference.y) * -1;
 
         _rb.AddForce(test);
+
+        _ShouldDrawLine = false;
+
+        _lineRenderer.enabled = false;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+
+        // Add a LineRenderer component
+        _lineRenderer = gameObject.AddComponent<LineRenderer>();
+
+        // Set the material
+        _lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+
+        // Set the color
+        _lineRenderer.startColor = Color.red;
+        _lineRenderer.endColor = Color.red;
+
+        // Set the width
+        _lineRenderer.startWidth = 0.02f;
+        _lineRenderer.endWidth = 0.01f;
+
+        // Set the number of vertices
+        _lineRenderer.positionCount = 2;
+
+        _lineRenderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_ShouldDrawLine)
+        {
+            // Get screen position
+            var screenPosition = _positionAction.ReadValue<Vector2>();
 
+            // Transform screen pos to world position
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y, Camera.main.nearClipPlane));
+            
+            Vector3 start = transform.position;
+
+            // Set the positions of the vertices
+            _lineRenderer.SetPosition(0, start);
+            _lineRenderer.SetPosition(1, worldPosition);
+        }
     }
 }
